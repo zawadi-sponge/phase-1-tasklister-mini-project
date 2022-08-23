f6dee6c772baebb024e3b3bb2292aecac8e26c94
Zawadi <zawadi.opoti@student.moringaschool.com>
2022-08-23 09:12:14 +0300

    assignment completed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  document.querySelector('form').addEventListener('Create New Task', (e) =>{
+    e.preventDefault();
+toDO(e.target.new_todo.value)
+  })
 });
